8075c6b8b886d22ca8bc930767322fa51508fb69
Brian Esser <brian.esser@escbilling.com>
2019-03-16 04:03:33 +0000

    npi.php

--- npi.php
+++ npi.php
@@ -4,6 +4,7 @@ $city = $_GET['city'];
 $state = $_GET['state'];
 $zip = $_GET['zip'];
 $taxonomy = $_GET['taxonomy'];
+$skip = $_GET['skip'];
 
 //echo "$city <br> $state <br> $zip <br> $taxonomy <br>";
 //die;
@@ -21,7 +22,17 @@ if($zip){
 if($taxonomy){
     $fields .= "&taxonomy_description=$taxonomy";
 }
-$url = "https://npiregistry.cms.hhs.gov/api?version=2.1$fields&pretty=false";
+
+
+$fetch = 0; //up to 1200
+$limit = 200;
+
+$data_array = [["FaxNumber", "To"]];
+$fax_duplicate_array = [];
+
+while($fetch < 1200){
+
+$url = "https://npiregistry.cms.hhs.gov/api?limit=$limit&version=2.1$fields&skip=$skip&pretty=false";
 //echo $url;
 //die;
     $ch = curl_init($url);
@@ -34,19 +45,30 @@ $url = "https://npiregistry.cms.hhs.gov/api?version=2.1$fields&pretty=false";
     //echo '<pre>' .$data.'</pre>';
     
     $data=json_decode($data);
-    $data_array = [["FaxNumber", "To"]];
+    
 
     foreach($data->results as $row){
         $npi_temp = $row->number;
         $fax_temp = $row->addresses[0]->fax_number;
         
+        
         if($fax_temp == ''){
+           // array_push($data_array, [$fax_temp, $npi_temp]);
+        }
+        else if(in_array($fax_temp, $fax_duplicate_array)){
             
         }
         else{
+        //echo $fax_temp ."<br>";
+        array_push($fax_duplicate_array, $fax_temp);
         array_push($data_array, [$fax_temp, $npi_temp]);
         }
     }
+    
+    $fetch = $fetch + 200;
+    $skip = $skip + 200;
+    
+}
   
 function array_to_csv_download($array, $filename = "export.csv", $delimiter=";") {
     header('Content-Type: application/csv');
